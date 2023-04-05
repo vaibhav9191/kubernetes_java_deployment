20f4c7130efe4a36d78db9d71024b76524443aa4
vaibhav9191 <68327363+vaibhav9191@users.noreply.github.com>
2023-04-05 12:48:41 +0530

    Update StockResource.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/resources/StockResource.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/resources/StockResource.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/stocks")
-@RequestMapping("/health")
 public class StockResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StockResource.class);
